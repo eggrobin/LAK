d44bb054892e905b166841fc8b32da4a154edde6
Robin Leroy <egg.robin.leroy@gmail.com>
2025-10-04 14:18:48 +0200

    Links, 𒊩𒆳

--- patch_font.py
+++ patch_font.py
@@ -1,6 +1,7 @@
 import fontforge
 
 SALT = "'salt' Stylistic Alternatives lookup 3 subtable"
+LIGA = "'liga' Standard Ligatures lookup 2 subtable"
 
 font = fontforge.open("LAK.ttf")
 
@@ -20,6 +21,13 @@ def add_alternate(code_point: str, glyph_name: str):
   print(f"added salt{len(salt)} to U+{ord(code_point):04X} {code_point}")
   font.buildOrReplaceAALTFeatures()
 
+def add_ligature(code_points: str, glyph_name: str):
+  if font[glyph_name].getPosSub(LIGA):
+    raise ValueError(f"{glyph_name} already has a LIGA mapping: {font[glyph_name].getPosSub(LIGA)}")
+  font[glyph_name].addPosSub(LIGA, tuple('u%X' % ord(code_point) for code_point in code_points))
+  print("liga:", " ".join('U+%X' % ord(code_point) for code_point in code_points), "↦", glyph_name)
+  font.buildOrReplaceAALTFeatures()
+
 # The index form comes from 12778 (a lexical text), whose dcclt/ebla
 # transliteration avoids reading them and calls them LAK011.  It makes sense to
 # hold off on identifying that one.  The other form of LAK011 form comes from
@@ -27,6 +35,8 @@ def add_alternate(code_point: str, glyph_name: str):
 add_alternate('𒆰', 'uF009C.1')
 # On LAK020, but transliterated bal in dcclt/ebla (in kuš-AN-ti-bal).
 add_alternate('𒁄', 'uF0086.1')
+# Note that the alternates also become 𒊩𒆳.
+add_ligature("𒊩𒆳", "uF00A0")
 assign('𒉋', "uF00B0")
 add_alternate('𒉋', 'uF00B0.1')
 add_alternate('𒉋', 'uF00B0.2')
