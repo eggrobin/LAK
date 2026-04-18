302deeb732c45ca01309bcba1280f2873670208a
Robin Leroy <egg.robin.leroy@gmail.com>
2026-04-18 04:29:58 +0200

    p. 68 and some of p. 69

--- patch_font.py
+++ patch_font.py
@@ -120,6 +120,7 @@ add_ligature("𒄷𒋛𒀀", "uF3900")
 add_ligature("𒑋𒁇", "uF3901")  # Or 𒑋𒁇?
 add_ligature("𒉿𒀀", "uF3904")
 add_ligature("𒈨𒋼", "uF3905") # LAK750
+add_ligature("𒇻𒄾", "uF3907") # LAK776, somehow not noted as a sequence.
 
 print("# pp. 11–15 corrections")
 add_ligature("𒋀𒆳𒊏", "uF00A5.1")
